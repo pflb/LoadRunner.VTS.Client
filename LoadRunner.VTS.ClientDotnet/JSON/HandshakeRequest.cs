7a0fd059b1d85419e26ae6371248f42154545c4f
Smirnov Vyacheslav Alexandrovich <Vyacheslav.SMIRNOV@partners.raiffeisen.ru>
2017-05-02 18:20:24 +0300

    refactoring - remove usings and references

--- LoadRunner.VTS.ClientDotnet/JSON/HandshakeRequest.cs
+++ LoadRunner.VTS.ClientDotnet/JSON/HandshakeRequest.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LoadRunner.VTSClientDotnet.JSON
+﻿namespace LoadRunner.VTSClientDotnet.JSON
 {
     class HandshakeRequest : AbstractRequest<HandshakeRequestData>
     {
