b5d67b10e70cad67e28420cacfb5357bb3196497
Smirnov Vyacheslav Alexandrovich <Vyacheslav.SMIRNOV@partners.raiffeisen.ru>
2017-09-05 17:11:48 +0300

    Add get raw test

--- UnitTestProject1/UnitTestVtsApiClass.cs
+++ UnitTestProject1/UnitTestVtsApiClass.cs
@@ -17,10 +17,27 @@ namespace UnitTestProject1
             VTS.connect("localhost", 8888, ConnectionOptions.None);
         }
 
+        /// <summary>
+        /// Get selected column
+        /// </summary>
         [TestMethod]
         public void VTS_rotate_messages()
         {
             string[] columns = {"CNUM", "SurName", "Name", "MiddleName", "BirthDay", "City" };
+
+            Dictionary<string, string> user = VTS.rotate_messages(columns, SendRow.Stacked);
+
+            Assert.AreNotEqual(user["SurName"], user["Name"]);
+        }
+
+        /// <summary>
+        /// Get all columns - get row
+        /// </summary>
+        [TestMethod]
+        public void VTS_rotate_messages_empty_columns()
+        {
+            
+            string[] columns = { };
 
             Dictionary<string, string> user = VTS.rotate_messages(columns, SendRow.Stacked);
 
